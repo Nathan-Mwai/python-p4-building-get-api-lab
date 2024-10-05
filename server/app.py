c162ed9e73d0c96682e11b687b48d34ad5668795
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-10-05 09:53:22 +0300

    finishes lab

--- server/app.py
+++ server/app.py
@@ -19,20 +19,42 @@ def index():
     return '<h1>Bakery GET API</h1>'
 
 @app.route('/bakeries')
+# Using list comprehension
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    
+    #Using normal looping
+     
+    # for bakery in Bakery.query.all():
+    #     baker_dict = bakery.to_dict()
+    #     bakeries.append(baker_dict)
+    response = make_response(bakeries,200,{"Content-Type": "application/json"})
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = bakery.to_dict()
+    response = make_response(bakery_dict, 200)
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = [baked.to_dict() for baked in BakedGood.query.order_by(BakedGood.price.desc()).all()]
+    
+    # for baked in BakedGood.query.order_by().all():
+    #     baked_dict = baked.to_dict()
+    #     baked_goods.append(baked_dict)
+    
+    response = make_response(baked_goods, 200)
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    baked_dict = baked.to_dict()
+    response = make_response(baked_dict, 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
